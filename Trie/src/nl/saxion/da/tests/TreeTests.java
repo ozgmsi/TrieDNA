1cf52d912782f82324dab49fdcebb1fd6ba42a0a
Ozgur <Ozgu.r@hotmail.com>
2015-01-09 20:17:19 +0100

    The trie

---
+++ Trie/src/nl/saxion/da/tests/TreeTests.java
@@ -0,0 +1,54 @@
+package nl.saxion.da.tests;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TreeTests {
+	//ClassBook<Persoon> graph = new ClassBook<Persoon>();
+	@Test
+	public void add() {
+		
+////		ClassBook<Persoon> graph = new ClassBook<Persoon>();
+//		Persoon persoon1 = new Persoon("Klaas", 1990, "klaas@email.nl");
+//		Persoon persoon2 = new Persoon("Klaas2", 1990, "klaas@email.nl");
+//		Persoon persoon3 = new Persoon("Klaas3", 1990, "klaas@email.nl");
+//		Persoon persoon4 = new Persoon("Klaas4", 1990, "klaas@email.nl");
+//		Persoon persoon5 = new Persoon("Klaas5", 1990, "klaas@email.nl");
+//		
+//		assert (graph.getFriends(persoon1).size() == 0) : "moet leeg zijn";
+//		graph.maakLid(persoon1);
+//		assert (graph.inClass(persoon1));
+//		assert (graph.getFriends(persoon1).size() == 1) : "moet  persoon1 bevatten";
+	}
+	@Test
+	public void remove()
+	{
+//		add();
+//		ClassBook<Persoon> graph = new ClassBook<Persoon>();
+//		Persoon persoon1 = new Persoon("Klaas", 1990, "klaas@email.nl");
+//		Persoon persoon2 = new Persoon("Klaas2", 1990, "klaas@email.nl");
+//
+//		
+//		assert (graph.getFriends(persoon1).size() == 0) : "moet persoon 1 bevatten";
+//		graph.maakLid(persoon1);
+//		assert (graph.inClass(persoon1));
+//		assert (graph.getFriends(persoon1).size() == 1) : "moet persoon 1 bevatten";
+//		graph.verwijderLid(persoon2);
+//		assert (graph.getFriends(persoon1).size() == 1) : "moet persoon 1 bevatten";
+//		graph.verwijderLid(persoon1);
+//		assert (graph.getFriends(persoon1).size() == 0) : "moet leeg zijn";
+	}
+	@Test
+	public void addFriend()
+	{
+//		add();
+//		graph.maakLid(new Persoon("Piet",1990,"piet@email.nl"));
+//		graph.maakLid(new Persoon("Klaas",1990,"piet@email.nl"));
+//		graph.addFriendShip(graph.getPersonByName("Piet"), graph.getPersonByName("Klaas"));
+//		assertTrue(graph.areFriends(graph.getPersonByName("Piet"), graph.getPersonByName("Klaas")));
+	}
+	
+	
+
+}
